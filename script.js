1c15e7813b7661dcb5fd3dd94e41c318c8f2655e
Ihsan Maulana <ikhsanmaulana000002@gmail.com>
2025-06-15 00:20:23 +0700

    Latihan: Menambahkan potensi code smell untuk SonarCloud

--- script.js
+++ script.js
@@ -17,10 +17,18 @@ function isOperator(value) {
 
 function clearDisplay() {
   display.value = "";
+
+  let unusedVariableForDemo = "hello"; // Variabel tidak digunakan
 }
 
+// Atau
+
 function deleteLast() {
-  display.value = display.value.slice(0, -1);
+  if (true) {
+    // Kondisi yang selalu benar
+
+    display.value = display.value.slice(0, -1);
+  }
 }
 
 function performCalculation(expression) {
